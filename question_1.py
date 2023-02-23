3ce71e2f8e2e9d36ed9e8ca341c97165b1cd3961
Brielle Bush <briellebush@Brielles-MacBook-Pro.local>
2023-02-23 11:45:54 -0800

    started question_1

---
+++ question_1.py
@@ -0,0 +1,12 @@
+import seaborn as sea
+import pandas as pd
+import matplotlib.pyplot as plot
+
+def read_data(file_name):
+    data = pd.read_csv("GLOB.SES.csv")
+
+def main(): 
+     
+
+# What impact does socioeconomic status have on national development?
+# To find this, we will look at the socioeconomic score of each country and compare it to the GDP per capita of that country. 
