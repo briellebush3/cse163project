bbc8dd89870c83f3f43a8fe4c4401dbc60dcdd15
Brielle Bush <briellebush@Brielles-MacBook-Pro.local>
2023-03-02 11:31:40 -0800

    added question 3 outline

--- question_1.py
+++ question_1.py
@@ -1,4 +1,4 @@
-import seaborn as sea
+import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plot
 
---
+++ question_2.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plot
+
+# find dataset about world education attainment (15+ year olds)
+# merge dataset with education column 
+# compare the new education column to the GDP column 
+# visualize using scatterplot 
