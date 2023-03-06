0a2a7eea421524cd5c47aad93adb32ac0be5f693
Brielle Bush <briellebush@Brielles-MacBook-Pro.local>
2023-03-06 14:42:59 -0800

    worked on README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# cse163project
+# CSE 163 Project - Socioeconomic Status Score by Country
+
+###### Project Instructions:
+1. Run question_1.py to answer our first research question, what factors contribute the most to the socioeconomic status score of a country?
+2. Run question_2.py to answer our second research question, how does education attainment contribute to the national development of a country via its GDP per capita?
+3. Run question_3.py to answer our third research question, how have the top 5 countries with the highest socioeconomic score and the bottom 5 countries with the lowest socioeconomic score changed from 1910-2010?
+
+
+######
+
+Your README.md should include:
+
+1. You should write instructions for us to run your project to reproduce your results. Tell us which Python modules to run to get your results and anything else we need to do to run them.
+2. If there is anything we need to do to set up your project, like install libraries or how to download your data (if you did not submit it), give us instructions for how to do so.
+3. Anything else we need to know about running your project!
+
+Your instructions should be detailed enough that your mentor can run your code to reproduce any of the results in your report. You can assume the reader of your instructions is familiar with programming environments in Python and have read your report. You should not assume your mentor will spend time “figuring out” how to run your project with anything outside of your instructions so make sure your instructions are clear and unambiguous.
--- question_2.py
+++ question_2.py
@@ -1,31 +1,30 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plot
+ 
 
-# find dataset about world education attainment (15+ year olds)
-# merge dataset with education column 
-# compare the new education column to the GDP column 
-# visualize using scatterplot 
-
-def GLOB_data(file_name):
+def new_data(file_name, file_name_2):
     data = pd.read_csv(file_name)
+    data2 = pd.read_csv(file_name_2)
 
-def new_data(file_name):
-    data = pd.read_csv(file_name)
-
-    df = data[['Country Name', 'Series Name', '1980 [YR1980]', '1990 [YR1990]', '2000 [YR2000]', '2010 [YR2010]']]
+    df = data2[['Country Name', 'Series Name', '1970 [YR1970]', '1980 [YR1980]' , '1990 [YR1990]' , '2000 [YR2000]' , '2010 [YR2010]']]
+   
 
     # filter for total primary and secondary school for 15+ 
-    total_educ = df['Series Name'] = "Barro-Lee: Average years of primary schooling, age 15+, total", "Barro-Lee: Average years of secondary schooling, age 15+, total"
-    total_educ = df[total_educ]
+    pri = df['Series Name'] == "Barro-Lee: Average years of primary schooling, age 15+, total"
+    sec = df['Series Name'] == "Barro-Lee: Average years of secondary schooling, age 15+, total" 
+    df = df[pri | sec]
+    print(df)
+
+    #total_educ = df[total_educ]
 
 # merge the datasets
-merged = GLOB_data.merge(total_educ, left_on='', right_on='')
+# merged = GLOB_data.merge(total_educ, left_on='', right_on='')
 
 
 def main(): 
-    GLOB_data("GLOB.SES.csv")
-    new_data("yrseduc_data2.csv")
+    new_data("GLOB.SES.csv", "yrseduc_data2.csv")
+    
 
 
 if __name__ == '__main__':
